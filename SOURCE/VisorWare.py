f2e9d462514c947c991e7e905ac6d45fe143bd54
Liam Charles <liamzcharles@gmail.com>
2018-08-19 21:41:22 +0400

    Initial update to VisorWare git repo.
    ^

---
+++ SOURCE/VisorWare.py
@@ -0,0 +1,86 @@
+###############################################################################
+#                                                                             #
+#     A project by:                                                           #
+#                     -----------------------------------                     #
+#                         L I A M  Z.  C H A R L E S                          #
+#                     -----------------------------------                     #
+#                                                                             #
+###############################################################################
+
+print('Launching VisorWare...\n')
+#                VisorWare BETA v0.1 || Built for Visor2.0                    #
+
+
+###############################################################################
+#                                                                             #
+#                       Copyright 2018 LIAM CHARLES                           #
+#                                                                             #
+#   Licensed under the Apache License, Version 2.0 (the "License");           #
+#   you may not use this file except in compliance with the License.          #
+#   You may obtain a copy of the License at                                   #
+#                                                                             #
+#               http://www.apache.org/licenses/LICENSE-2.0                    #
+#                                                                             #
+#   Unless required by applicable law or agreed to in writing, software       #
+#   distributed under the License is distributed on an "AS IS" BASIS,         #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#   See the License for the specific language governing permissions and       #
+#    limitations under the License.                                           #
+#                                                                             #
+###############################################################################
+
+import time
+import RPi.GPIO as GPIO
+import os
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from termCol import *
+
+#######################################
+# Display Initialization. DO NOT ALTER!
+
+RST = 24
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+disp.begin()
+width = disp.width
+height = disp.height
+disp.clear()
+disp.display()
+
+#
+#######################################
+
+
+##################################################
+# Button input board initialization. DO NOT ALTER!
+
+GPIO.setmode(GPIO.BCM)
+downb = 5
+okayb = 6
+upb = 13
+GPIO.setup(downb, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(okayb, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(upb, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#
+##################################################
+
+os.system('clear')
+print ('                     -----------------------------------')
+print ('                         L I A M  Z.  C H A R L E S')                          
+print ('                     -----------------------------------') 
+
+print ('\n\n\nVisorWare v0.1\n\n')
+print ('THIS IS A BETA VERSION. BEWARE OF BUGS.')
+print ('Proper functionality cannot be guaranteed in a BETA build of VisorWare. Please install a stable version of VisorWare for stable and proper functionality.\n\n')
+
+print ('IN DEV')
---
+++ SOURCE/termCol.py
@@ -0,0 +1,90 @@
+# Terminal-Color module v1.0.
+# Packaged with VisorWare, a project by Liam Z. Charles.
+
+class Base:
+    # Foreground:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    # Formatting
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'    
+    # End colored text
+    END = '\033[0m'
+    NC ='\x1b[0m' # No Color
+
+class ANSI_Compatible:
+    END = '\x1b[0m'
+    # If Foreground is False that means color effect on Background
+    def Color(ColorNo, Foreground=True): # 0 - 255
+        FB_G = 38 # Effect on foreground
+        if Foreground != True:
+            FB_G = 48 # Effect on background
+        return '\x1b[' + str(FB_G) + ';5;' + str(ColorNo) + 'm'
+
+class Formatting:
+    Bold = "\x1b[1m"
+    Dim = "\x1b[2m"
+    Italic = "\x1b[3m"
+    Underlined = "\x1b[4m"
+    Blink = "\x1b[5m"
+    Reverse = "\x1b[7m"
+    Hidden = "\x1b[8m"
+    # Reset part
+    Reset = "\x1b[0m"
+    Reset_Bold = "\x1b[21m"
+    Reset_Dim = "\x1b[22m"
+    Reset_Italic = "\x1b[23m"
+    Reset_Underlined = "\x1b[24"
+    Reset_Blink = "\x1b[25m"
+    Reset_Reverse = "\x1b[27m"
+    Reset_Hidden = "\x1b[28m"
+
+class GColor: # Gnome supported
+    END = "\x1b[0m"
+    # If Foreground is False that means color effect on Background
+    def RGB(R, G, B, Foreground=True): # R: 0-255  ,  G: 0-255  ,  B: 0-255
+        FB_G = 38 # Effect on foreground
+        if Foreground != True:
+            FB_G = 48 # Effect on background
+        return "\x1b[" + str(FB_G) + ";2;" + str(R) + ";" + str(G) + ";" + str(B) + "m"
+
+class Color:
+    # Foreground
+    F_Default = "\x1b[39m"
+    F_Black = "\x1b[30m"
+    F_Red = "\x1b[31m"
+    F_Green = "\x1b[32m"
+    F_Yellow = "\x1b[33m"
+    F_Blue = "\x1b[34m"
+    F_Magenta = "\x1b[35m"
+    F_Cyan = "\x1b[36m"
+    F_LightGray = "\x1b[37m"
+    F_DarkGray = "\x1b[90m"
+    F_LightRed = "\x1b[91m"
+    F_LightGreen = "\x1b[92m"
+    F_LightYellow = "\x1b[93m"
+    F_LightBlue = "\x1b[94m"
+    F_LightMagenta = "\x1b[95m"
+    F_LightCyan = "\x1b[96m"
+    F_White = "\x1b[97m"
+    # Background
+    B_Default = "\x1b[49m"
+    B_Black = "\x1b[40m"
+    B_Red = "\x1b[41m"
+    B_Green = "\x1b[42m"
+    B_Yellow = "\x1b[43m"
+    B_Blue = "\x1b[44m"
+    B_Magenta = "\x1b[45m"
+    B_Cyan = "\x1b[46m"
+    B_LightGray = "\x1b[47m"
+    B_DarkGray = "\x1b[100m"
+    B_LightRed = "\x1b[101m"
+    B_LightGreen = "\x1b[102m"
+    B_LightYellow = "\x1b[103m"
+    B_LightBlue = "\x1b[104m"
+    B_LightMagenta = "\x1b[105m"
+    B_LightCyan = "\x1b[106m"
+    B_White = "\x1b[107m"
